6373b4ec0b14112efeb3f9554bb2dd66ff9f9586
Joker 281 <dennismoldovan32@gmail.com>
2023-06-20 19:02:29 +0300

    env

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -7,14 +7,14 @@ export async function POST(req:NextRequest){
     try{
         const props = await req.json();
         
-        const dataObject = await axios.get('http://localhost:3000/api/getProducts');
+        const dataObject = await axios.get('https://stripe-payment-415ny4m3h-dennisdingo28.vercel.app/api/getProducts');
 
         const data = dataObject.data;
         if(process.env.STRIPE_SECRET_KEY){
             const stripe = new Stripe(process.env.STRIPE_SECRET_KEY,{
                 apiVersion:"2022-11-15",
             });
-            const user = await axios.post('http://localhost:3000/api/getUser',props);
+            const user = await axios.post('https://stripe-payment-415ny4m3h-dennisdingo28.vercel.app/api/getUser',props);
             
             const session = await stripe.checkout.sessions.create({
                 line_items:[{
@@ -26,8 +26,8 @@ export async function POST(req:NextRequest){
                 payment_method_types:["card"],
                 customer:user.data.payment.customerId,
               
-                success_url:"http://localhost:3000/dashboard",
-                cancel_url:"http://localhost:3000/dashboard"
+                success_url:"https://stripe-payment-415ny4m3h-dennisdingo28.vercel.app/dashboard",
+                cancel_url:"https://stripe-payment-415ny4m3h-dennisdingo28.vercel.app/dashboard"
             });
             return NextResponse.json({url:session.url,ok:true});
             
